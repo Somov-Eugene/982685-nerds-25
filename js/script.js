e6f3ea872e26eb4a1a129737abcf48f178ac4be7
Somov Eugene <somov.eugene.2012@gmail.com>
2019-06-11 23:12:32 +0300

    добавлена интерактивная карта

--- js/script.js
+++ js/script.js
@@ -79,11 +79,52 @@ text.addEventListener("click", function (evt) {
 });
 
 window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+  if (evt.keyCode === 27) { // клавиша ESC
     evt.preventDefault();
+
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
       popup.classList.remove("modal-error");
     }
   }
+});
+
+
+// Yandex map
+ymaps.ready(function () {
+  var myMap = new ymaps.Map("map", {
+    center: [59.93912633838372, 30.322121091262822],
+    zoom: 17,
+    controls: ["rulerControl"]
+  });
+
+  myMap.controls.add("zoomControl", {
+    float: "none",
+    position: {
+        bottom: "100px",
+        right: "10px"
+    }
+  });
+
+  myMap.controls.add("fullscreenControl", {
+    float: "none",
+    position: {
+        bottom: "50px",
+        right: "10px"
+    }
+  });
+
+  myMap.controls.remove("rulerControl");
+
+  var myPlacemark = new ymaps.Placemark([59.938777700539546, 30.3229872462131], {
+    hintContent: "Студия «Нёрдс» находится здесь",
+    balloonContent: "Ждём Вас по адресу: Санкт-Петербург, ул. Большая Конюшенная, д. 19/8"
+  }, {
+    iconLayout: "default#image",
+    iconImageHref: "img/map-marker.png",
+    iconImageSize: [231, 190],
+    iconImageOffset: [-45, -180]
+  });
+
+  myMap.geoObjects.add(myPlacemark);
 });
